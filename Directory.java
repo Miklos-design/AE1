c11ba411c3d0cf7de5ded0c81ed89dd05873ee04
Miki-design <73119146+Miklos-design@users.noreply.github.com>
2024-02-17 16:08:36 +0000

    Your commit message here

---
+++ Directory.java
@@ -0,0 +1,102 @@
+package file2930017;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory implements Component {
+    private String name;
+    private List<Component> fileList;
+    private static String initialPrefix = "";
+    private String folderPrefix="";
+
+    public void setFolderPrefix(String folderPrefix) {
+        this.folderPrefix = folderPrefix;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+
+    @Override
+    public int getSize() {
+        int totalSize = 0;
+        for (Component f : fileList) {
+            totalSize += f.getSize();
+        }
+        return totalSize;
+    }
+
+    @Override
+    public int getCount() {
+        int count = 0;
+        for (Component f : fileList) {
+            count += f.getCount();
+        }
+        return count;
+    }
+    public Directory(String name) {
+        this.name = name;
+        this.fileList = new ArrayList<>();
+    }
+    public void add(Component c) {
+        fileList.add(c);
+    }
+
+    public void remove(Component c) {
+        fileList.remove(c);
+    }
+
+    @Override
+    public String display(String initialPrefix) {
+        // Set the static initial prefix
+        this.initialPrefix = initialPrefix;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(folderPrefix).append(this);
+
+        // Call the method for recursion and append its result to the string builder
+        displayContents(stringBuilder, folderPrefix);
+
+        return stringBuilder.toString();
+    }
+
+    private void displayContents(StringBuilder stringBuilder, String folderPrefix) {
+        // Append initial prefix for subdirectories
+        folderPrefix += initialPrefix;
+        for (Component f : fileList) {
+            // Append the file or directory with the amended prefix
+            stringBuilder.append("\n").append(folderPrefix).append(f);
+            if (f instanceof Directory) {
+                ((Directory) f).setFolderPrefix(folderPrefix);
+                ((Directory) f).displayContents(stringBuilder, folderPrefix );
+            }
+        }
+    }
+
+    @Override
+    public Component search(String name) {
+        for (Component component : fileList) {
+            if (component!=null &&
+                    component instanceof File &&
+                    component.getName().equals(name)) {
+            return this;
+        }   else if (component instanceof Directory) {
+            Component found = ((Directory) component).search(name);
+            if (found != null) {
+                return found;
+            }
+        }
+    }
+    return null;
+    }
+
+    @Override
+    public String toString() {
+        return  name + ": (count=" + getCount() + ", size=" + getSize() + ")";
+    }
+
+
+}
+
+
---
+++ File.java
@@ -0,0 +1,50 @@
+package file2930017;
+
+public class File implements Component {
+    private String name;
+    private int size;
+    private int count;
+
+
+    public File(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public int getCount() {
+        return 1;
+    }
+
+
+    @Override
+    public Component search(String name) {
+        return this.name.equals(name) ? this : null;
+    }
+
+    @Override
+    public String display(String prefix) {
+        return "\n"+ prefix + toString() ;
+    }
+
+
+    @Override
+    public String toString( ) {
+        return name  + "("+ size +")" ;
+    }
+
+}
